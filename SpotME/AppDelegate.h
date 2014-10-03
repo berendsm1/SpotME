f83633389a39f1fbe829fe0aa7eae69fdbe84ee1
Martijn <fridoon@Martijn-Berends-Macbook.local>
2014-10-03 20:50:01 +0200

    Initial Commit

---
+++ SpotME/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotME
+//
+//  Created by Martijn on 03/10/14.
+//  Copyright (c) 2014 Martijn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
